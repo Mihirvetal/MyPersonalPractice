96cf0038c1fb4d451064093f29223fbd90ee5453
Mihir Vetal <151329052+Mihirvetal@users.noreply.github.com>
2024-10-13 15:42:59 +0530

    Day2

---
+++ node.js/Day2/index.js
@@ -0,0 +1,28 @@
+// const {readFileSync, writeFileSync} = require('fs');
+// const {add} = require('./calc');
+// writeFileSync("new.txt" , 'mihir', 'utf-8');
+// const read = readFileSync("new.txt", 'utf-8');
+// console.log(read);
+// const addd = add(2,2);
+// console.log(addd);
+
+const http = require('http');
+
+
+const server  = http.createServer();
+
+server.listen(5000, (req,res)=>{
+    console.log(<h1>mihir</h1>);
+});
+
+
+
+
+
+
+
+
+
+
+
+
