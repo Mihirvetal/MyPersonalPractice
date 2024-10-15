37a60e70838c7e7150b28637cae4b5f2c512e62c
Mihir Vetal <151329052+Mihirvetal@users.noreply.github.com>
2024-10-15 10:17:50 +0530

    Done for Day2

--- node.js/Day2/index.js
+++ node.js/Day2/index.js
@@ -1,4 +1,4 @@
-// const {readFileSync, writeFileSync} = require('fs');
+const {readFileSync, writeFileSync, read} = require('fs');
 // const {add} = require('./calc');
 // writeFileSync("new.txt" , 'mihir', 'utf-8');
 // const read = readFileSync("new.txt", 'utf-8');
@@ -8,15 +8,35 @@
 
 const http = require('http');
 
+//  so creating server using http and give the response of file in server response with readFileSync 
+// const server  = http.createServer((req,res)=>{
+//     try{
+//         const data = readFileSync('./new.txt','utf-8')
+//         res.end(data)
+//     }catch(err){
+//         res.end('cant read file')
+//     }
+// });
 
-const server  = http.createServer();
+// server.listen(5000, ()=>{
+//     console.log('http://localhost:5000');
+// });
 
-server.listen(5000, (req,res)=>{
-    console.log(<h1>mihir</h1>);
-});
+const server = http.createServer((req,res)=>{
+    if (req.url === '/hi'){
+        return res.end("hi")
+    }
+    if (req.url === '/hello'){
+        return res.end('hello')
+    }
+    if (req.url === '/user/masai'){
+        return res.end(JSON.stringify([1,2,3,4,5]))
+    }
+})
 
-
-
+server.listen(5000 , (req,res)=>{
+    console.log("http://localhost:5000");
+})
 
 
 
