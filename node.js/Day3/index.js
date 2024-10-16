fae0fdf96c2d37d0cf4c7ef309198835b42a0b96
Mihir Vetal <151329052+Mihirvetal@users.noreply.github.com>
2024-10-16 23:59:54 +0530

    Done Day3

--- node.js/Day3/index.js
+++ node.js/Day3/index.js
@@ -3,23 +3,23 @@ const express = require('express')
 
 const app = express()
 
-app.get('/',(req,res)=>{
-    return res.end("get index")
-})
+// app.get('/',(req,res)=>{
+//     return res.end("get index")
+// })
 
 // send html to the browser with server 
-app.get('/html', (req,res)=>{
-    return res.send('<h1 style="font-size:58px">Mihir</h1>')
-})
-app.post('/',(req,res)=>{
-    return res.end("post index 1")
-})
-app.get('/date', (req, res) => {
-    return res.send(new Date().toUTCString()); // Sends the current date
-});
+// app.get('/html', (req,res)=>{
+//     return res.send('<h1 style="font-size:58px">Mihir</h1>')
+// })
+// app.post('/',(req,res)=>{
+//     return res.end("post index 1")
+// })
+// app.get('/date', (req, res) => {
+//     return res.send(new Date().toUTCString()); // Sends the current date
+// });
 
 // for getting that particular id  & its called dynamic route
-// path params = Path parameters are dynamic segments of a URL that allow you to capture values from the URL path and pass them to your route handlers in an Express.js application. 
+// path params = Path parameters are dynamic segments of a URL that allow you to capture values from the URL path and pass them to your route handlers in an Express.js application. it give only the value which you request as a response.
 app.get('/user/:userid', (req,res)=>{
     const id = req.params.userid;
     return res.send({
@@ -27,9 +27,13 @@ app.get('/user/:userid', (req,res)=>{
     })
 })
  
-// query params = Query parameters are a way to pass additional data in the URL of a request, typically used in GET requests. They are appended to the URL after a question mark (?) and consist of key-value pairs, separated by the ampersand (&).
+// query params = Query parameters are a way to pass additional data in the URL of a request, typically used in GET requests. They are appended to the URL after a question mark (?) and consist of key-value pairs, separated by the ampersand (&). it always give the object as response
  
-
+app.get('/ha', (req,res)=>{
+    const query = req.query
+    console.log("query",query);
+    return res.send("ok")
+})
 
 
 
