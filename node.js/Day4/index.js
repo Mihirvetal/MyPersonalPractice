c832a044fb7b9d69a77c83284bddb5a9e2266515
Mihir Vetal <151329052+Mihirvetal@users.noreply.github.com>
2024-10-17 10:18:23 +0530

    Done

--- node.js/Day4/index.js
+++ node.js/Day4/index.js
@@ -4,11 +4,16 @@
 const express = require('express')
 const app = express();
 
-app.use((req , res)=>{
-    
-})  // use to create the MW or like a global MW
+// yeh jo {use} he voh har baar call or response dega or yeh dusri request or response ko accept nahi hone dega Q ki yeh middleware callback he 
+app.use((req , res,next)=>{
+    console.log("object" , req.url);
+    return res.send("Done")
+    // next use hota he ki agar hume is {use} ka response nahi chaiye or iske baad ke response chaiye toh hum next() MW ka use karte he or yeh ek callback hota he
+    next()
 
-app.get('/', (req,res)=>{
+})  // {use} to create the MW or like a global MW
+
+app.get('/1', (req,res)=>{
     return res.send('<p style="font-size:120px">Mihir</p>')
 })
 
