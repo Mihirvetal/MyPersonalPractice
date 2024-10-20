54fa5ff9afbd4a9b11f844887f121f8305094673
Mihir Vetal <151329052+Mihirvetal@users.noreply.github.com>
2024-10-20 20:13:19 +0530

    Day6 Done

--- node.js/Day5/db/connection.js
+++ node.js/Day5/db/connection.js
@@ -2,4 +2,4 @@ const mongoose = require("mongoose");
 
 const connection = mongoose.connect("mongodb://localhost:27017/imdb")
 
-module.exports = connection 
+module.exports = connection;
--- node.js/Day5/routes/movieRouter.js
+++ node.js/Day5/routes/movieRouter.js
@@ -1,17 +1,30 @@
-const {Router} = require('express')
 
+
+const {Router} = require('express')
+const Moviemodel = require('../models/moviemodel')
 const movierouter = Router();
 
-movierouter.get('/movies', (req,res)=>{
+movierouter.get('/movies', async (req,res)=>{
+    const movies = await Moviemodel.find({})
     return res.json({
-        list:[1,2,3,4]
+        list:movies,
     })
 })
 
-// movierouter.get('/:id', (req,res)=>{
-//     return res.json({
-//         movie:{}
-//     })
-// })
+
+
+movierouter.get('/:id', (req,res)=>{
+    return res.json({
+        movie:movies
+    })
+})
+
+
+// post method to add movies in database
+movierouter.post('/addmovie' , async (req,res) =>{
+    const data = req.body;
+    await Moviemodel.insertMany(data);
+    return res.send('movie added')
+})
 
 module.exports = movierouter;
