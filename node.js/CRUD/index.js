e6b6bb1c2ae5ddc58675e136446487faa895d951
Mihir Vetal <151329052+Mihirvetal@users.noreply.github.com>
2024-11-09 17:50:35 +0530

    CRUD AUTH DONE

--- node.js/CRUD/index.js
+++ node.js/CRUD/index.js
@@ -2,12 +2,32 @@ const express = require("express");
 const app = express();
 const connection = require('./db/connection')
 const todoRouter = require('./routes/todo.route')
+const userRouter = require('./routes/user.route')
+const jwt = require("jsonwebtoken")
 
 app.use(express.json());
+const SECRETKEY = "habhai"
 
+
+app.use('/',userRouter)
 app.use('/', todoRouter)
+app.use((req,res,next)=>{
+    const token = req.headers.authorization;
+    if(!token){
+        return res.status(401).send("invalid user")
+    }
+    try{
+        jwt.verify(token,SECRETKEY);
+        next()
+    }catch{
+        return res.status(401).send("invalid token")
+    }
+    
+})
 
-
+app.get('/aboutpage',(req,res)=>{
+    return res.send("about page this")
+})
 app.listen(3000, async ()=>{
     await connection
     console.log("http://localhost:3000");
---
+++ node.js/CRUD/models/user.model.js
@@ -0,0 +1,19 @@
+const {Schema,model} = require("mongoose");
+
+const userSchema = new Schema({
+    name: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const userModel = model("user", userSchema)
+
+
+module.exports = userModel;
